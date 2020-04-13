8a462683b177e2089e7fedfc4e33c8d7da35ee8d
SebastianOpiyo <sebamopale@gmail.com>
2020-04-13 12:43:30 +0300

    Challange 1 complete.

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    const { reportedCases } = input;
+    
+    // Challange 1.
+    const notSevereImpact = {
+        currentlyInfected : (reportedCases) => reportedCases * 10,
+        infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512
+    }
+    
+    const severeCasesImpact = {
+        currentlyInfected : (reportedCases) => reportedCases * 50,
+        infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512
+    }
+
+    // Challange 2.
+
+    return {
+        data: input,
+        impact: notSevereImpact,
+        severeImpact: severeCasesImpact
+    };
+};
+
 
 export default covid19ImpactEstimator;
