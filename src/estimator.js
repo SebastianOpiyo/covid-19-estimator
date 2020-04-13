307817b793fb2439f3977ac0b5cb002e008c58d4
SebastianOpiyo <sebamopale@gmail.com>
2020-04-13 19:49:40 +0300

    fix indentation errors.

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,28 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    const { reportedCases, totalHospitalBeds } = input;
-    
-    // Challange 1.
-    const notSevereImpact = {
-        currentlyInfected : (reportedCases) => reportedCases * 10,
-        infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512,
-        severeCasesByRequestedTime: (infectionsByRequestedTime) => 0.15 * infectionsByRequestedTime,
-        hospitalBedsByRequestedTime: (severeCasesByRequestedTime, totalHospitalBeds) => Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,     
-    }
-    
-    const severeCasesImpact = {
-        currentlyInfected : (reportedCases) => reportedCases * 50,
-        infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512,
-        severeCasesByRequestedTime: (infectionsByRequestedTime) => 0.15 * infectionsByRequestedTime,
-        hospitalBedsByRequestedTime: (severeCasesByRequestedTime, totalHospitalBeds) => (0.35 * totalHospitalBeds) - severeCasesByRequestedTime,  
-    }
+  const input = data;
+  // const { reportedCases, totalHospitalBeds } = input;
+
+  // Challange 1.
+  const notSevereImpact = {
+      currentlyInfected : (reportedCases) => reportedCases * 10,
+      infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512,
+      severeCasesByRequestedTime: (infectionsByRequestedTime) => 0.15 * infectionsByRequestedTime,
+      hospitalBedsByRequestedTime: (severeCasesByRequestedTime, totalHospitalBeds) => Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+  }
+
+  const severeCasesImpact = {
+      currentlyInfected : (reportedCases) => reportedCases * 50,
+      infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512,
+      severeCasesByRequestedTime: (infectionsByRequestedTime) => 0.15 * infectionsByRequestedTime,
+      hospitalBedsByRequestedTime: (severeCasesByRequestedTime, totalHospitalBeds) => (0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+  }
 
 
-    return {
-        data: input,
-        impact: notSevereImpact,
-        severeImpact: severeCasesImpact
-    };
+  return {
+      data: input,
+      impact: notSevereImpact,
+      severeImpact: severeCasesImpact
+  };
 };
 
 
