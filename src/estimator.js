ef098f21c6d09d2fe9546f2ffb1c8a9ddc94fc27
SebastianOpiyo <sebamopale@gmail.com>
2020-04-13 20:41:11 +0300

    Removed functionsand used variables only.

--- src/estimator.js
+++ src/estimator.js
@@ -1,27 +1,42 @@
+// Sample data format representation.
+// var data = {
+//     region: {
+//     name: "Africa",
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: "days",
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+//     }
+
+
 const covid19ImpactEstimator = (data) => {
-  const input = data;
-  // const { reportedCases, totalHospitalBeds } = input;
+  const { reportedCases, totalHospitalBeds } = data;
 
   // Challange 1.
   const notSevereImpact = {
-      currentlyInfected : (reportedCases) => reportedCases * 10,
-      infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512,
-      severeCasesByRequestedTime: (infectionsByRequestedTime) => 0.15 * infectionsByRequestedTime,
-      hospitalBedsByRequestedTime: (severeCasesByRequestedTime, totalHospitalBeds) => Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+      currentlyInfected:reportedCases * 10,
+      infectionsByRequestedTime:currentlyInfected * 512,
+      severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
+      hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
   }
 
   const severeCasesImpact = {
-      currentlyInfected : (reportedCases) => reportedCases * 50,
-      infectionsByRequestedTime: (currentlyInfected) => currentlyInfected * 512,
-      severeCasesByRequestedTime: (infectionsByRequestedTime) => 0.15 * infectionsByRequestedTime,
-      hospitalBedsByRequestedTime: (severeCasesByRequestedTime, totalHospitalBeds) => (0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+      currentlyInfected:reportedCases * 50,
+      infectionsByRequestedTime:currentlyInfected * 512,
+      severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
+      hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
   }
 
 
   return {
-      data: input,
-      impact: notSevereImpact,
-      severeImpact: severeCasesImpact
+      data:data,
+      impact:notSevereImpact,
+      severeImpact:severeCasesImpact
   };
 };
 
