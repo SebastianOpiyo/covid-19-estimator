e5cf412a1ef00ef107d6f342499dd360fc9b6a32
SebastianOpiyo <sebamopale@gmail.com>
2020-04-13 20:57:21 +0300

    Fix lint error.

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,26 @@
 const covid19ImpactEstimator = (data) => {
-const { reportedCases, totalHospitalBeds } = data;
+  const { reportedCases, totalHospitalBeds } = data;
 
-const notSevereImpact = {
-  currentlyInfected:reportedCases * 10,
-  infectionsByRequestedTime:currentlyInfected * 512,
-  severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
-  hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
-}
+  const notSevereImpact = {
+    currentlyInfected:reportedCases * 10,
+    infectionsByRequestedTime:currentlyInfected * 512,
+    severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
+    hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+  }
 
-const severeCasesImpact = {
-  currentlyInfected:reportedCases * 50,
-  infectionsByRequestedTime:currentlyInfected * 512,
-  severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
-  hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
-}
+  const severeCasesImpact = {
+    currentlyInfected:reportedCases * 50,
+    infectionsByRequestedTime:currentlyInfected * 512,
+    severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
+    hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+  }
 
 
-return {
-  data:data,
-  impact:notSevereImpact,
-  severeImpact:severeCasesImpact
-};
+  return {
+    data:data,
+    impact:notSevereImpact,
+    severeImpact:severeCasesImpact
+  };
 };
 
 
