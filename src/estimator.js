ac54ef2363e05d5fbea754d25b81b4e2cd545086
SebastianOpiyo <sebamopale@gmail.com>
2020-04-13 22:22:08 +0300

    Fix all lint issues in challanges done so far.

--- src/estimator.js
+++ src/estimator.js
@@ -1,25 +1,38 @@
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, totalHospitalBeds } = data;
+  // Destructure the data object
+
+  // Infected not severe
+  const currentlyInfected = ({ reportedCases }) => reportedCases * 10;
+  const infectionsByRequestedTime = (curInfected = currentlyInfected()) => curInfected * 512;
+  const severeCasesByRequestedTime = (infec = infectionsByRequestedTime()) => Math.trunc(
+    0.15 * infec
+  );
+  const hospitalBedsByRequestedTime = ({ totalHospitalBeds }, sev =
+  severeCasesByRequestedTime()) => (Math.trunc(0.35 * totalHospitalBeds) - sev);
+
+  // Infected and Severe Cases.
+  const currentlyInfectedSevere = ({ reportedCases }) => reportedCases * 50;
+
 
   const notSevereImpact = {
-    currentlyInfected:reportedCases * 10,
-    infectionsByRequestedTime:currentlyInfected * 512,
-    severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
-    hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
-  }
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime
+  };
 
   const severeCasesImpact = {
-    currentlyInfected:reportedCases * 50,
-    infectionsByRequestedTime:currentlyInfected * 512,
-    severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
-    hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
-  }
+    currentlyInfected: currentlyInfectedSevere,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime
+  };
 
 
   return {
-    data:data,
-    impact:notSevereImpact,
-    severeImpact:severeCasesImpact
+    data,
+    impact: notSevereImpact,
+    severeImpact: severeCasesImpact
   };
 };
 
