8806a7418c44c728f0e5b433c5d33ec8853e996c
SebastianOpiyo <sebamopale@gmail.com>
2020-04-13 20:47:19 +0300

    Fix spacing.

--- src/estimator.js
+++ src/estimator.js
@@ -1,43 +1,26 @@
-// Sample data format representation.
-// var data = {
-//     region: {
-//     name: "Africa",
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//     },
-//     periodType: "days",
-//     timeToElapse: 58,
-//     reportedCases: 674,
-//     population: 66622705,
-//     totalHospitalBeds: 1380614
-//     }
-
-
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, totalHospitalBeds } = data;
+const { reportedCases, totalHospitalBeds } = data;
 
-  // Challange 1.
-  const notSevereImpact = {
-      currentlyInfected:reportedCases * 10,
-      infectionsByRequestedTime:currentlyInfected * 512,
-      severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
-      hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
-  }
+const notSevereImpact = {
+  currentlyInfected:reportedCases * 10,
+  infectionsByRequestedTime:currentlyInfected * 512,
+  severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
+  hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+}
 
-  const severeCasesImpact = {
-      currentlyInfected:reportedCases * 50,
-      infectionsByRequestedTime:currentlyInfected * 512,
-      severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
-      hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
-  }
+const severeCasesImpact = {
+  currentlyInfected:reportedCases * 50,
+  infectionsByRequestedTime:currentlyInfected * 512,
+  severeCasesByRequestedTime:Math.trunc(0.15 * infectionsByRequestedTime),
+  hospitalBedsByRequestedTime:Math.trunc(0.35 * totalHospitalBeds) - severeCasesByRequestedTime,
+}
 
 
-  return {
-      data:data,
-      impact:notSevereImpact,
-      severeImpact:severeCasesImpact
-  };
+return {
+  data:data,
+  impact:notSevereImpact,
+  severeImpact:severeCasesImpact
+};
 };
 
 
