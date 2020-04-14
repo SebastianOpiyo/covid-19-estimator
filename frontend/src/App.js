f9d6a3e9503ee9f5c0bdcb1e7e4f29b54b80d206
SebastianOpiyo <sebamopale@gmail.com>
2020-04-14 04:11:08 +0300

    Add react frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Covid-19 Novel Estimator App</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/estimator.js
+++ src/estimator.js
@@ -9,7 +9,8 @@ const covid19ImpactEstimator = (data) => {
   const hospitalBedsByRequestedTime = ({ totalHospitalBeds }, sev =
   severeCasesByRequestedTime()) => (Math.trunc(0.35 * totalHospitalBeds) - sev);
 
-
+// Missing days, weeks, or months incorporation
+// Need to refine challage 2
   const notSevereImpact = {
     currentlyInfected,
     infectionsByRequestedTime,
@@ -34,3 +35,15 @@ const covid19ImpactEstimator = (data) => {
 
 
 export default covid19ImpactEstimator;
+
+// challange 3
+/*
+1. Determine 5% of infectionsByRequestedTime =
+   severe +ve cases estimate for ICU : casesForICUByRequestedTime(estimation output)
+2. Determine 2% of infectionsByRequestedTime = 
+  severe +ve for ventilators: casesForVentilatorsByRequestedTime
+3. With infected people result and avg income of the region estimate how much the economy 
+   is about to lose daily over the given period of time: dolarInFlight  
+*/
+
+//     "babel-jest": "^25.2.0",
